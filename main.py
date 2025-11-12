9945ae82d3f8282ecf9350776f587c98c243d9c7
gavinnepo <gavinnepo@gmail.com>
2025-11-12 17:57:32 +0000

    some notes for cs11

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+'''name = input("what is your name?")
+print ("hello, " + name)'''
+
+''' data types 
+Classification for the different kinds of data available in 
+programming language
+
+#int (dealing w whole numbers)
+#float (decimal numbers)
+#string ()
+deci_ans = round(14/65,2)
+string1 = "hiii"
+You need the quotes to make it strings''' 
+
+answer = input("What is 14+ 65?")
+if (answer == 79) {
+    print("correct")
+} else if (answer > 79) {
+    print("wrong)")
+} else {
+    print("wrong")
+}
 
