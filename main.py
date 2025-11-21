bd0132c023a866d1f89febb7271b5712d0d96cdb
gavinnepo <gavinnepo@gmail.com>
2025-11-21 03:35:25 +0000

    cs11 assess again

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("hello, world!")
-name = input("what is your name?")
-print(name)
+print("Hello this is the superduper cool gav calculator")
+
+radius = float(input("What is thy radius sir?"))
+area = radius*radius*3.14
+print ("this is your area I think " + str(area))
             
